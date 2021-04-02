f44277b34cbbe2106f1ff3126d8b1602c01af118
芝法酱 <hataksumo@163.com>
2021-04-02 17:38:08 +0800

    上传代码

---
+++ reactive-web/src/main/java/indi/zhifa/learn/reactiveweb/api/CallExtApi.java
@@ -0,0 +1,11 @@
+package indi.zhifa.learn.reactiveweb.api;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/alarmType")
+public class CallExtApi {
+}
